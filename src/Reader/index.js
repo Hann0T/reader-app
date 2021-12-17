ab58463d8032df13e12c24cd9c4d5ea0f60bd2b4
hann0t <hansolivas1@gmail.com>
2021-12-16 21:59:06 -0500

    Increase/decrease fontsize functionality added. Reader component stateful.

--- src/Reader/index.js
+++ src/Reader/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { ReadButton } from '../ReadButton';
+import { ReaderUI } from './ReaderUI';
 
 import './Reader.css';
 
@@ -18,18 +18,31 @@ const Reader = ({ toggleModal, text, numberOfWords }) => {
     if (!position > 0) return;
     setPosition((prevState) => prevState - numberOfWords);
   };
+
   const nextWord = () => {
     if (position > length) return;
     setPosition((prevState) => prevState + numberOfWords);
   };
 
+  const increaseFontsize = () => {
+    if (fontsize >= 10) return;
+    setFontsize((prevState) => prevState + 1);
+  };
+
+  const decreaseFontsize = () => {
+    if (fontsize <= 2) return;
+    setFontsize((prevState) => prevState - 1);
+  };
+
   const keyDown = (ev) => {
     if (ev.key === 'ArrowLeft') return prevWord();
     if (ev.key === 'ArrowRight') return nextWord();
+    if (ev.key === 'ArrowUp') return increaseFontsize();
+    if (ev.key === 'ArrowDown') return decreaseFontsize();
     if (ev.key === 'Escape') return toggleModal();
   };
 
-  const setText = () => {
+  const renderText = () => {
     let localText = arrayWords[position];
 
     for (let i = 1; i < numberOfWords; i++) {
@@ -39,16 +52,6 @@ const Reader = ({ toggleModal, text, numberOfWords }) => {
 
     if (!localText) return setTextToShow('type something');
     setTextToShow(localText);
-  };
-
-  const incrementFontsize = () => {
-    if (fontsize >= 10) return;
-    setFontsize((prevState) => prevState + 1);
-  };
-
-  const decrementFontsize = () => {
-    if (fontsize <= 2) return;
-    setFontsize((prevState) => prevState - 1);
   };
 
   React.useEffect(() => {
@@ -61,31 +64,21 @@ const Reader = ({ toggleModal, text, numberOfWords }) => {
 
   React.useEffect(() => {
     keyInput.current.focus();
-    setText();
+    renderText();
   });
 
   return (
-    <section tabIndex='0' ref={keyInput} onKeyDown={keyDown} className='reader'>
-      <div className='container'>
-        <ReadButton
-          onClickButton={toggleModal}
-          customClass={'button-absolute'}
-          text={'x'}
-        />
-        <div className='button-container'>
-          <ReadButton onClickButton={incrementFontsize} text={'+'} />
-          <p>{fontsize}</p>
-          <ReadButton onClickButton={decrementFontsize} text={'-'} />
-        </div>
-        <p className='reader-text' style={{ fontSize: `${fontsize}em` }}>
-          {textToShow}
-        </p>
-        <div className='button-container'>
-          <ReadButton onClickButton={prevWord} text={'prev'} />
-          <ReadButton onClickButton={nextWord} text={'next'} />
-        </div>
-      </div>
-    </section>
+    <ReaderUI
+      keyInput={keyInput}
+      keyDown={keyDown}
+      toggleModal={toggleModal}
+      increaseFontsize={increaseFontsize}
+      decreaseFontsize={decreaseFontsize}
+      fontsize={fontsize}
+      textToShow={textToShow}
+      prevWord={prevWord}
+      nextWord={nextWord}
+    />
   );
 };
 
