5dc79f32d86d6dcbbeb14123d1c4a88017a76212
hann0t <hansolivas1@gmail.com>
2021-12-20 19:26:47 -0500

    some changes to the timer component.

--- src/Timer/index.js
+++ src/Timer/index.js
@@ -1,38 +1,21 @@
 import React from 'react';
 import { useStopwatch } from 'react-timer-hook';
 
-  
-  export { Timer };
-  const Timer = ( ) => {
-  const {
-    seconds,
-    minutes,
-    hours,
-    days,
-    isRunning,
-    start,
-    pause,
-    reset,
-  } = useStopwatch({ autoStart: true });
-
+const Timer = () => {
+  const { seconds, minutes, hours, days, isRunning } = useStopwatch({
+    autoStart: true,
+  });
 
   return (
-    <div style={{textAlign: 'center'}}>
-
+    <div style={{ textAlign: 'center' }}>
       <p>Timer</p>
-      <div style={{fontSize: '14px'}}>
-        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+      <div style={{ fontSize: '14px' }}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:
+        <span>{seconds}</span>
       </div>
       <p>{isRunning ? 'Running' : 'Not running'}</p>
-
     </div>
   );
-}
+};
 
-export default function App() {
-  return (
-    <div>
-      <Timer />
-    </div>
-  );
-}
+export { Timer };
