fa41ca96769754f20bade6c77bd23c8092382790
hann0t <hansolivas1@gmail.com>
2021-12-20 20:24:50 -0500

    changes.

--- src/Timer/index.js
+++ src/Timer/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { useStopwatch } from 'react-timer-hook';
 
 const Timer = () => {
-  const { seconds, minutes, hours, days, isRunning } = useStopwatch({
+  const { seconds, minutes, hours, days } = useStopwatch({
     autoStart: true,
   });
 
   return (
     <div style={{ textAlign: 'center' }}>
-      <p>Timer</p>
+      <p>Tiempo</p>
       <div style={{ fontSize: '14px' }}>
         <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:
         <span>{seconds}</span>
       </div>
-      <p>{isRunning ? 'Running' : 'Not running'}</p>
     </div>
   );
 };
