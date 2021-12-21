1991a0970f72c56baf500831929bcf64f6929bb8
hann0t <hansolivas1@gmail.com>
2021-12-20 20:24:42 -0500

    new components.

---
+++ src/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='container'>
+        <h1>App Lector</h1>
+      </div>
+    </header>
+  );
+};
+
+export { Header };
