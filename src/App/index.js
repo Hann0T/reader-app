8f86ee9e33227444e20b553858e74211af28a21d
hann0t <hansolivas1@gmail.com>
2021-12-20 21:38:18 -0500

    some changes in the App component.

--- src/App/index.js
+++ src/App/index.js
@@ -1,59 +1,16 @@
 import React from 'react';
 
-import { Modal } from '../Modal';
-import { Button } from '../Button';
-import { Reader } from '../Reader';
+import { Header } from '../Header';
+import { ReaderInput } from '../ReaderInput';
 
 import './App.css';
 
 function App() {
-  const [openModal, setOpenModal] = React.useState(false);
-  const [text, setText] = React.useState('');
-  const [numberOfWords, setNumberOfWords] = React.useState(1);
-
-  const onChangeTextArea = (ev) => {
-    let text = ev.target.value;
-    setText(text);
-  };
-  const onChangeInput = (ev) => {
-    let number = parseInt(ev.target.value);
-    setNumberOfWords(number);
-  };
-
-  const onClickButton = () => {
-    setOpenModal((prevState) => !prevState);
-  };
-
   return (
-    <section className='presentation'>
-      <div className='container'>
-        <input
-          className='number-input'
-          value={numberOfWords}
-          type='number'
-          onChange={onChangeInput}
-          min='1'
-        />
-        <textarea
-          value={text}
-          onChange={onChangeTextArea}
-          placeholder='tu texto...'
-          className='text-input'
-        />
-        <div className='button-container'>
-          <Button onClickButton={onClickButton} text={'Leer Texto'} />
-        </div>
-      </div>
-      {openModal && (
-        <Modal>
-          <Reader
-            text={text}
-            numberOfWords={numberOfWords}
-            toggleModal={onClickButton}
-          />
-        </Modal>
-      )}
-    </section>
+    <>
+      <Header />
+      <ReaderInput />
+    </>
   );
 }
 
